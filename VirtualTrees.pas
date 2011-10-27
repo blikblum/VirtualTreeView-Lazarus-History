db86f4f26063003e61ed32a43e8991ece87bd7a5
sekelsenmat <sekelsenmat@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2011-10-27 08:26:45 +0000

    Fixes the compilation
    git-svn-id: svn+ssh://svn.code.sf.net/p/lazarus-ccr/svn/components/virtualtreeview-new/trunk@2097 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- VirtualTrees.pas
+++ VirtualTrees.pas
@@ -15120,11 +15120,13 @@ var
   ScrollLines: DWORD;
   RTLFactor: Integer;
   WheelFactor: Double;
-
+  State: TShiftState;
 begin
   //todo: rename to WM*
   {$ifdef DEBUG_VTV}Logger.EnterMethod([lcScroll],'CMMouseWheel');{$endif}
   StopWheelPanning;
+
+  State := KeysToShiftState(Message.Button);
 
   inherited WMMouseWheel(Message);
 
