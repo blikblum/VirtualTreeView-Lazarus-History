e9a4433024d8ce4e8ab9a7d6536753b4e59475dd
blikblum <blikblum@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2016-05-22 14:30:49 +0000

    *Update VTLogger to compile after recent changes in MultiLog
    git-svn-id: svn+ssh://svn.code.sf.net/p/lazarus-ccr/svn/components/virtualtreeview-new/branches/4.8@4657 8e941d3f-bd1b-0410-a28a-d453659cc2b4

--- virtualtreeview_package.pas
+++ virtualtreeview_package.pas
@@ -2,8 +2,9 @@
   This source is only used to compile and install the package.
  }
 
-unit virtualtreeview_package; 
+unit virtualtreeview_package;
 
+{$warn 5023 off : no warning about unused units}
 interface
 
 uses
@@ -12,11 +13,11 @@ uses
 
 implementation
 
-procedure Register; 
+procedure Register;
 begin
-  RegisterUnit('registervirtualtreeview', @registervirtualtreeview.Register); 
-end; 
+  RegisterUnit('registervirtualtreeview', @registervirtualtreeview.Register);
+end;
 
 initialization
-  RegisterPackage('virtualtreeview_package', @Register); 
+  RegisterPackage('virtualtreeview_package', @Register);
 end.
